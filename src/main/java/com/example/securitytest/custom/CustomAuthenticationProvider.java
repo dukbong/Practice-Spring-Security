fd9d5a91b20b853f64d698a5e21fdbdddde6e188
janghs <gorae930301@gmail.com>
2024-05-09 16:40:05 +0900

    공백 수정

--- src/main/java/com/example/securitytest/custom/CustomAuthenticationProvider.java
+++ src/main/java/com/example/securitytest/custom/CustomAuthenticationProvider.java
@@ -20,30 +20,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 	
-    private final CustomUserDetailsService customAddUrlUserDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final CustomUserDetailsService customAddUrlUserDetailsService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
 	@Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String password = (String) authentication.getCredentials();
-        String requestUrl = (String) authentication.getDetails(); 
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = (String) authentication.getCredentials();
+		String requestUrl = (String) authentication.getDetails(); 
 
-        UserDetails userDetails = customAddUrlUserDetailsService.loadUserByUsernameAndUrl(username, requestUrl);
+		UserDetails userDetails = customAddUrlUserDetailsService.loadUserByUsernameAndUrl(username, requestUrl);
         
-        if (!bCryptPasswordEncoder.matches(password, userDetails.getPassword())) {
-            throw new BadCredentialsException("유효하지 않은 비밀번호 입니다!!");
-        }
+		if (!bCryptPasswordEncoder.matches(password, userDetails.getPassword())) {
+			throw new BadCredentialsException("유효하지 않은 비밀번호 입니다!!");
+		}
         
-        // Url 로직 추가 예정
+		// Url 로직 추가 예정
         
-        return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
-    }
+		return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+	}
 
 	@Override
-    public boolean supports(Class<?> authentication) {
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
-    }
+	public boolean supports(Class<?> authentication) {
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
 
 }
