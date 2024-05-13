27550718f41317f7d26ced38cb8b0e851552fb61
janghs <gorae930301@gmail.com>
2024-05-13 16:48:29 +0900

    비밀번호 유효성 검사

---
+++ src/main/java/com/example/securitytest/enums/PasswordRegex.java
@@ -0,0 +1,17 @@
+package com.example.securitytest.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PasswordRegex {
+
+	EMAIL_REGEX("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$"),
+	UPPERCASE(".*[A-Z].*"),
+	LOWERCASE(".*[a-z].*"),
+	SPECIAL_CHARACTER(".*[!@#$%^&*()\\-_=+\\\\|\\[{\\]};:'\",<.>/?].*");
+	
+	private String regex;
+	
+}
--- src/main/java/com/example/securitytest/serviceImpl/JoinServiceImpl.java
+++ src/main/java/com/example/securitytest/serviceImpl/JoinServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.securitytest.dto.JoinDTO;
 import com.example.securitytest.entity.UserEntity;
 import com.example.securitytest.entity.UserEntityId;
+import com.example.securitytest.enums.PasswordRegex;
 import com.example.securitytest.repository.UserEntityRepository;
 import com.example.securitytest.service.JoinService;
 
@@ -27,12 +28,35 @@ public class JoinServiceImpl implements JoinService {
 		if(duplicateUserNameAndAccessUrl) {
 			throw new IllegalArgumentException("Duplicate UserName or AccessUrl");
 		}
+		validateUserPwd(joinDTO.getPassWord());
 		
 		UserEntity userEntity = UserEntity.builder().id(UserEntityId.builder().userName(joinDTO.getUserName()).accessUrl(joinDTO.getAccessUrl()).build())
 											        .passWord(bCryptPasswordEncoder.encode(joinDTO.getPassWord()))
 											        .role("ROLE_B")
 											        .build();
 		userEntityRepository.save(userEntity);
+	}
+	
+	private void validateUserPwd(String pwd) {
+		if (pwd == null || pwd.isEmpty()) {
+			throw new IllegalArgumentException("비밀번호는 비어있을 수 없습니다.");
+		}
+
+		if (pwd.length() < 8) {
+			throw new IllegalArgumentException("비밀번호는 8자 이상이여야 합니다.");
+		}
+
+		if (!pwd.matches(PasswordRegex.UPPERCASE.getRegex())) {
+			throw new IllegalArgumentException("비밀번호는 대문자가 한개 이상 포함되어야 합니다.");
+		}
+
+		if (!pwd.matches(PasswordRegex.LOWERCASE.getRegex())) {
+			throw new IllegalArgumentException("비밀번호는 소문자가 한개 이상 포함되어야 합니다.");
+		}
+
+		if (!pwd.matches(PasswordRegex.SPECIAL_CHARACTER.getRegex())) {
+			throw new IllegalArgumentException("비밀번호는 특수문자가 한개 이상 포함되어야 합니다.");
+		}
 	}
 
 }
